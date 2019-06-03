3099cd3a4f202af16ca1341c49a97acc9dc9ce96
Joel Luth <joel.luth@dell.comm>
2019-06-02 20:32:14 -0500

    add test for note.py

---
+++ tests/test_note.py
@@ -0,0 +1,22 @@
+import pytest
+import sys
+
+sys.path.insert(0, '../lib')
+import note
+
+def test_note_freq_too_low():
+    with pytest.raises(ValueError):
+        test = note.Note(-1)
+
+def test_note_freq_ok():
+    test = note.Note(440)
+    assert test.freq == 440
+
+def test_note_freq_too_high():
+    with pytest.raises(ValueError):
+        test = note.Note(100 * 1024 * 1024)
+
+def test_note_freq_change():
+    test = note.Note(440)
+    test.freq = 450
+    assert test.freq == 450
