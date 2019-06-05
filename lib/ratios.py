f131c2cf4a713fdaba2c2c1381931be7dbab94b1
Joel Luth <joel.luth@dell.comm>
2019-06-04 22:24:45 -0500

    basic ratios functions
    tuning-specifc functions will go in separate files

---
+++ lib/ratios.py
@@ -0,0 +1,13 @@
+"""
+Mathematical ratios for musical calculations
+"""
+
+from math import log2
+
+OCTAVE_CENTS = 1200
+
+def cents(freq1, freq2):
+    return OCTAVE_CENTS * log2(freq2 / freq1)
+
+def freq_ratio(cents):
+    return 2 ** (cents / OCTAVE_CENTS)
