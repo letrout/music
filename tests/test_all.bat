b78a6077217bae6cf748bc73ca954a9b7dedfe60
Joel Luth <joel.luth@dell.comm>
2019-09-22 17:11:29 -0500

    add scripts to run all unit tests

---
+++ tests/test_all.bat
@@ -0,0 +1,4 @@
+pytest -q test_note.py
+pytest -q test_scale.py
+pytest -q test_scale_octave.py
+pytest -q test_scale_12edo.py
---
+++ tests/test_all.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+pytest -q test_note.py
+pytest -q test_scale.py
+pytest -q test_scale_octave.py
+pytest -q test_scale_12edo.py
