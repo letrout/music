c0d908af716f43bc677afb279dc22e4d42a5d1fd
Joel Luth <joel.luth@dell.comm>
2019-09-22 15:27:43 -0500

    add degrees property checks to unit tests

--- tests/test_scale_octave.py
+++ tests/test_scale_octave.py
@@ -7,11 +7,13 @@ import scale_octave
 def test_scale_tone1200():
     test = scale_octave.ScaleOctave()
     retval = test.add_tone(1200)
-    assert retval == 1
+    assert retval == 2
+    assert test.degrees == [1, 2]
     assert test.tones == [0, 1200]
 
 def test_scale_tone_overmax():
     test = scale_octave.ScaleOctave()
     retval = test.add_tone(scale_octave.OCTAVE_CENTS + 1)
     assert retval is None
+    assert test.degrees == [1]
     assert test.tones == [0]
