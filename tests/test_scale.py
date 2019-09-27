bb458484a57ae6a78daca9db77ce02ffe4f9526b
Joel Luth <joel.luth@dell.comm>
2019-09-26 20:19:53 -0500

    add some unit tests
    new dataframe methods require more unit tests

--- tests/test_scale.py
+++ tests/test_scale.py
@@ -231,3 +231,23 @@ def test_move_degree_root():
     assert retval == -1
     assert test.degrees == [1, 2, 3, 4, 5, 6, 7, 8]
     assert test.tones == [0, 200, 400, 500, 700, 900, 1100, 1200]
+
+def test_tone_from_degree():
+    test = scale.Scale(tones=[200, 400, 500, 700, 900, 1100, 1200])
+    assert test.tone_from_degree(3) == 400
+
+def test_tone_from_degree_nonexist():
+    test = scale.Scale(tones=[200, 400, 500, 700, 900, 1100, 1200])
+    assert test.tone_from_degree(15) is None
+
+def test_degree_from_tone():
+    test = scale.Scale(tones=[200, 400, 500, 700, 900, 1100, 1200])
+    assert test.degree_from_tone(400) == 3
+
+def test_degree_from_tone_nonexist():
+    test = scale.Scale(tones=[200, 400, 500, 700, 900, 1100, 1200])
+    assert test.degree_from_tone(201) is None
+
+def test_degree_tones():
+    test = scale.Scale(tones=[200, 400, 500])
+    assert test.degree_tones == {1: 0, 2: 200, 3: 400, 4: 500}
