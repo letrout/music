4bfd0fd41cd974d1a396a739e3fae62428f076b7
Joel Luth <joelluth@gmail.com>
2022-02-14 15:30:22 -0600

    parameterize pytests

--- tests/test_midi_12edo.py
+++ tests/test_midi_12edo.py
@@ -11,56 +11,62 @@ import pytest
 import lib.midi_12edo as midi_12edo
 
 
-def test_notes_df_halfstep():
+@pytest.mark.parametrize(
+    'index, rel, expected',
+    [
+        (3, 'halfstep', 3),
+        (7, 'note_sharp', 'G'),
+        (10, 'note_flat', 'B'),
+        (5, 'accidental', 0)
+    ],
+)
+def test_notes_df(index, rel, expected):
     test = midi_12edo.notes_df()
-    assert test.iloc[3]['halfstep'] == 3
+    assert test.iloc[index][rel] == expected
 
 
-def test_notes_df_note_sharp():
-    test = midi_12edo.notes_df()
-    assert test.iloc[7]['note_sharp'] == 'G'
+@pytest.mark.parametrize(
+    'midi, , octave',
+    [
+        (5, -1),
+        (21, 0),
+        (30, 1),
+        (51, 3)
+    ],
+)
+def test_octave_from_midi(midi, octave):
+    test = midi_12edo.octave_from_midi(midi)
+    assert test == octave
 
 
-def test_notes_df_note_flat():
-    test = midi_12edo.notes_df()
-    assert test.iloc[10]['note_flat'] == 'B'
+@pytest.mark.parametrize(
+    'midi, , freq',
+    [
+        (69, 440)
+    ],
+)
+def test_freq_from_midi(midi, freq):
+    test = midi_12edo.freq_from_midi(midi)
+    assert test == freq
 
 
-def test_notes_df_accidental():
-    test = midi_12edo.notes_df()
-    assert test.iloc[5]['accidental'] == 0
+@pytest.mark.parametrize(
+    'freq, , midi',
+    [
+        (440, 69)
+    ],
+)
+def test_midi_from_freq(freq, midi):
+    test = midi_12edo.midi_from_freq(freq)
+    assert test == midi
 
 
-def test_octave_from_midi_vlow():
-    test = midi_12edo.octave_from_midi(5)
-    assert test == -1
-
-
-def test_octave_from_midi_low():
-    test = midi_12edo.octave_from_midi(21)
-    assert test == 0
-
-
-def test_octave_from_midi_mid():
-    test = midi_12edo.octave_from_midi(30)
-    assert test == 1
-
-
-def test_octave_from_midi_high():
-    test = midi_12edo.octave_from_midi(51)
-    assert test == 3
-
-
-def test_freq_from_midi():
-    test = midi_12edo.freq_from_midi(69)
-    assert test == 440
-
-
-def test_midi_from_freq():
-    test = midi_12edo.midi_from_freq(440)
-    assert test == 69
-
-
-def test_piano_key_from_midi():
-    test = midi_12edo.piano_key_from_midi(108)
-    assert test == 88
+@pytest.mark.parametrize(
+    'midi, , key',
+    [
+        (100, 80)
+    ],
+)
+def test_piano_key_from_midi(midi, key):
+    test = midi_12edo.piano_key_from_midi(midi)
+    assert test == key
